a1479d9b4a7677777b04b3965a43ffb05be3e49d
Jeff Rose <rosejn@jive.localdomain>
2006-11-21 16:49:09 +0100

    Adding small benchmark program.

---
+++ test/benchmark.rb
@@ -0,0 +1,37 @@
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+
+require 'gosim'
+require 'rubygems'
+require 'benchmark'
+
+class Benchmarker < GoSim::Entity
+  attr_reader :counter
+
+  def initialize(n)
+    super()
+
+    @counter = 0
+
+    n.times do |t|
+      @sim.schedule_event(:item, @sid, t * 10, t)
+    end
+  end
+
+  def handle_item(t)
+    @counter += 1
+  end
+end
+
+NUM_EVENTS = 100000
+
+sim = GoSim::Simulation::instance
+
+Benchmark.bm do |stat|
+  puts "Starting benchmark for #{NUM_EVENTS} events:\n"
+
+  b = Benchmarker.new(NUM_EVENTS)
+  stat.report { sim.run }
+
+  puts "Benchmark complete after counting #{b.counter} events.\n"
+end
+
